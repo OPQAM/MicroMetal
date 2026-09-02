3ffc3a1605785811ad3bb06e78ffcb69496e2b8b
OPQAM <theopqam@protonmail.com>
2026-09-02 21:42:06 +0100

    Finished 3rd experiment. Need to cleanup an use notes, etc

--- Projects/MemoryMapping/The_Fork/experiment_C_different_virtual_addr.c
+++ Projects/MemoryMapping/The_Fork/experiment_C_different_virtual_addr.c
@@ -29,6 +29,11 @@ int main(void)
     
     int shm_fd = shm_open("/my_memory", O_CREAT | O_RDWR, 0600);
 
+    if (shm_fd == -1) {
+        perror("shm_open");
+        return 1;
+    }
+
     if (ftruncate(shm_fd, size) == -1) {
         perror("ftruncate");
         return 1;
@@ -56,6 +61,11 @@ int main(void)
         perror("mmap");
         return 1;
     }
+    
+    if (addr2 == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
 
     uintptr_t virtual_address = (uintptr_t)addr;
     uintptr_t virtual_address2 = (uintptr_t)addr2;
@@ -71,17 +81,22 @@ int main(void)
     printf("PID: %d\n", pid);
    
     uintptr_t page_offset = virtual_address & 0xFFF; 
-    
+    uintptr_t page_offset2 = virtual_address2 & 0xFFF; 
+
     printf("Page offset: 0x%lx\n", page_offset);
     
     uintptr_t virtual_page = virtual_address >> 12; 
+    uintptr_t virtual_page2 = virtual_address2 >> 12; 
 
-    printf("Virtual Page: %lu\n", virtual_page);
+    printf("Virtual Page 1: %lu\n", virtual_page);
+    printf("Virtual Page 2: %lu\n", virtual_page2);
    
     
     off_t pagemap_offset = virtual_page * 8;
+    off_t pagemap_offset2 = virtual_page2 * 8;
     
-    printf("Pagemap offset: %ld\n", pagemap_offset);
+    printf("Pagemap offset 1: %ld\n", pagemap_offset);
+    printf("Pagemap offset 2: %ld\n", pagemap_offset2);
     
     char path[64];
 
@@ -105,23 +120,41 @@ int main(void)
         perror("read");
         return 1;
     }
+    
+    if (lseek(fd, pagemap_offset2, SEEK_SET) == -1) {
+        perror("lseek");
+        return 1;
+    }
 
-    printf("Pagemap entry: 0x%lx\n", entry);
+    uint64_t entry2;
+
+    if (read(fd, &entry2, sizeof(entry2)) != sizeof(entry2)) {
+        perror("read");
+        return 1;
+    }
+
+    printf("Pagemap entry 1: 0x%lx\n", entry);
+    printf("Pagemap entry 2: 0x%lx\n", entry2);
     
     uint64_t pfn = entry & 0x007FFFFFFFFFFFFF;
+    uint64_t pfn2 = entry2 & 0x007FFFFFFFFFFFFF;
 
-    printf("PFN: 0x%lx\n", pfn);
+    printf("PFN 1: 0x%lx\n", pfn);
+    printf("PFN 2: 0x%lx\n", pfn2);
     
     uint64_t physical_address = (pfn * size) + page_offset;
+    uint64_t physical_address2 = (pfn2 * size) + page_offset2;
     
-    printf("Physical address: 0x%lx\n", physical_address);
+    printf("Physical address 1: 0x%lx\n", physical_address);
+    printf("Physical address 2: 0x%lx\n", physical_address2);
 
     int user_char = getchar();
 
     if (user_char == '\n') {
         munmap(addr, size);
+        munmap(addr2, size);
     }
-
+    shm_unlink("/my_memory");
     close(fd);
 
     return 0;
